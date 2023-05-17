214381cb50c8b9ebbad5ac7f82469e07bcbbb61c
Akshata Kanaje <akshatakanaje@Akshatas-MacBook-Pro.local>
2023-05-16 17:14:57 -0700

    added home page and images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignInComponent } from './components/auth/sign-in/sign-in.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { FoodItemsComponent } from './components/food-items/food-items.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component: HomeComponent},
+  {path:'home' , component: HomeComponent},
+  {path:'contactus' , component: ContactusComponent},
+  {path:'aboutus' , component: AboutusComponent},
+  {path:'orders', component:OrdersComponent},
+  {path:'feedback', component:FeedbackComponent},
+  {path: 'auth', children : [
+    {path: 'login', component: LoginComponent},
+    {path: 'sign-in', component: SignInComponent},
+  ]},
+  {path: 'food-items', component: FoodItemsComponent},
+  {path:'**', component: NotFoundComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { ContactusComponent } from './components/contactus/contactus.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { FoodItemsComponent } from './components/food-items/food-items.component';
 import { CartComponent } from './components/cart/cart.component';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NavbarComponent,
     ContactusComponent,
     AboutusComponent,
     FoodItemsComponent,
     CartComponent,
     HomeComponent,
+    NotFoundComponent,
+    OrdersComponent,
+    FeedbackComponent,
 
   ],
   imports: [
